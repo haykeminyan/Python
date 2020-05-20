c86927e4247b7a2b9122375259195dd29f477398
HaykEminyan <ibhayk@gmail.com>
2020-05-20 15:20:40 +0300

    final

--- Test_Oblako_group/example/example.feature
+++ Test_Oblako_group/example/example.feature
@@ -1,9 +1,7 @@
-#Укажем что это за фича
 Feature: Checking search
-#Укажем имя сценария (в одной фиче может быть несколько)
 @fixture.browser.chrome
 Scenario: Сheck some text in search results
 #И используем наши шаги.
-  Given website "http://qa-assignment.oblakogroup.ru/board/:idhaykeminyan#"
+  Given website "http://qa-assignment.oblakogroup.ru/board/:idhaykeminyan#", '200'
   Then page include text 'Задачи','Семья','Новая задача'
   Then push button with text 'Ок'
--- Test_Oblako_group/steps/behave_test.py
+++ Test_Oblako_group/steps/behave_test.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 from behave import *
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from behave import given, when, then, step  # pylint: disable=no-name-in-module
+import requests
 
-def before_tag(context, tag):
-    if tag == "fixture.browser.chrome":
-        use_fixture(browser_chrome, context)
 
-# проверяем верный ли браузер открыли
 
 @fixture
 def browser_chrome(context, timeout=30, **kwargs):
@@ -15,17 +13,25 @@ def browser_chrome(context, timeout=30, **kwargs):
     context.browser = "chrome"
     yield
     print("FIXTURE-CLEANUP: browser.chrome")
+def before_tag(context, tag):
+    if tag == "fixture.browser.chrome":
+        use_fixture(browser_chrome, context)
 
 
-
+    
+    
+# открываем браузер, расширяем на всё окно и открываем ссылку. Проверяем статус кода
 @given("website {url}")
 def step(context,url):
     context.browser=webdriver.Chrome()
     context.browser.maximize_window()
-    context.browser.get('http://qa-assignment.oblakogroup.ru/board/:idhaykeminyan#')
+    href_addr='http://qa-assignment.oblakogroup.ru/board/:idhaykeminyan#'
+    context.browser.get(href_addr)
+    assert requests.get(href_addr).status_code
 
-# открываем браузер, расширяем на всё окно и открываем ссылку
 
+
+# проверяем через assert, что есть слова "задачи","Семья". Дальше кликаем на кнопку и проверяем есть ли слово "Новая задача"
 @then("page include {text} {text_1} {text_2}")
 def step(context, text,text_1,text_2):
     assert context.browser.find_element_by_css_selector('.title'.format(text))
@@ -33,10 +39,9 @@ def step(context, text,text_1,text_2):
     context.browser.find_element_by_css_selector('#add_new_todo').click()
     assert context.browser.find_element_by_css_selector('.add_white_block > h3:nth-child(1)')
 
-# проверяем через assert, что есть слова "задачи","Семья". Дальше кликаем на кнопку и проверяем есть ли слово "Новая задача"
+# проверяем есть ли слово "Ок" и кликаем на него
 @then("push button with text '{text}'")
 def step(context, text):
     assert context.browser.find_element_by_css_selector('#submit_add_todo'.format(text))
     context.browser.find_element_by_css_selector('#submit_add_todo').click()
     context.browser.quit()
-# проверяем есть ли слово "Ок" и кликаем на него
