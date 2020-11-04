1675cfe1614d32709c9c6ac6071c4745061457f4
haykeminyan <ibhayk@gmail.com>
2020-11-04 20:05:10 +0300

    final

--- Algorithms and data structures/1/linear_search.py
+++
@@ -1,8 +0,0 @@
-def linear_search(list_arr, target):
-    for i,j in enumerate(list_arr):
-        if j == target:
-            return i
-    return -1    
-
-print(linear_search([1,2,3,4,5],32))
-print(linear_search([3,2,1,43,2],43))
